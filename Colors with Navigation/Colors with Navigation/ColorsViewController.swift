a726272eb5d345a350cdbb41907530b95f454c57
harrill1 <hharrill12@gmail.com>
2018-07-12 18:53:55 -0600

    Added base project
    Currently is the same as the Colors app

---
+++ Colors with Navigation/Colors with Navigation/ColorsViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ColorsViewController.swift
+//  Colors with Navigation
+//
+//  Created by Hayden on 7/12/18.
+//  Copyright © 2018 Harrill, Hayden. All rights reserved.
+//
+
+import UIKit
+
+class ColorsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
+    var colorsForUI = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple, UIColor.brown]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        
+        cell.textLabel?.text = colors[indexPath.row]
+        cell.backgroundColor = colorsForUI[indexPath.row]
+        
+        
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
